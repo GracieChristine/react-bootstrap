5a3899c7c20b5b7436593a44c9218f0527dcb1b0
GracieChristine <graciechristineli@gmail.com>
2017-08-03 17:17:42 -0600

    Tried Grids.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-// import Buttons from "./Buttons.jsx";
-// import Dropdowns from "./Dropdowns.jsx";
-// import NavigationNavs from "./NavigationNavs.jsx";
-// import NavigationNavbars from "./NavigationNavbars.jsx";
-// import NavigationBreadcrumbs from "./NavigationBreadcrumbs.jsx";
-// import NavigationTabs from "./NavigationTabs.jsx";
-import NavigationPager from "./NavigationPager.jsx";
+import Grids from "./PageLayout/Grid.jsx";
 
 render (
-  // <Buttons />,
-  // <Dropdowns />,
-  // <NavigationNavs />,
-  // <NavigationNavbars />,
-  // <NavigationBreadcrumbs />,
-  // <NavigationTabs />,
-  <NavigationPager />,
+  <Grids />,
   document.getElementById("app")
 );
