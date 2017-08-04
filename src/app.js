8375abdc22ae3c25ebee0939626b7000ed786452
GracieChristine <graciechristineli@gmail.com>
2017-08-04 00:49:05 -0600

    Tried some animation.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-import ProgressBars from "./Miscellaneous/ProgressBars.jsx";
+import Transitions from "./Utilities/Transitions.jsx";
 
 render (
-  <ProgressBars />,
+  <Transitions />,
   document.getElementById("app")
 );
